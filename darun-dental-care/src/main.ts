91d8537d72760db6e7b81c0446e5295000d04c4e
Mohideen <mohideen.b@oceaniasolution.com>
2025-02-24 20:10:08 +0530

    feat(deleter)

--- darun-dental-care/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { provideRouter } from '@angular/router';
-import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
-
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)], // Provide routing
-}).catch((err) => console.error(err));
